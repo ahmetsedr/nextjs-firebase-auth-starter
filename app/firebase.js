ed74cd1bea0d2e9f9b9b4f550740121382afcf51
Ahmetsedr <ahmetsedur@gmail.com>
2024-04-14 11:01:39 +0300

    firebaseNextjsV2

--- app/firebase.js
+++ app/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCY7BFXSeF7VyVcWC1fIuBtoj2OJP4iMGE",
-  authDomain: "deneme-d850a.firebaseapp.com",
-  projectId: "deneme-d850a",
-  storageBucket: "deneme-d850a.appspot.com",
-  messagingSenderId: "791588001351",
-  appId: "1:791588001351:web:9e0dcd72213808521ec362"
+ 
 };
 
 // Initialize Firebase
